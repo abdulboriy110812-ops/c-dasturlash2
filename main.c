6d8ebce266c66659359199bae4aa134a952ff4e9
Abdulboriy <abdulboriy110812@gmail.com>
2026-07-24 10:14:05 +0500

    5-dars

--- main.c
+++ main.c
@@ -46,19 +46,50 @@ void mashq3(void) {
      int xujjat = 1;
 
      if(yosh >= 18 && xujjat == 1){
-        printf("Mashina xaydashga ruxsat");
+        printf("Mashina xaydashga ruxsat\n");
      }else{
-        printf("Mashina xaydashga ruxsat emas");
+        printf("Mashina xaydashga ruxsat emas\n");
      }
-
+     
 }
 
+void mashq4(void){
+     int hafta_kuni = 2;
+     switch (hafta_kuni)
+     {
+     case 1:
+        printf("Dushanba");
+        break;
+     case 2:
+        printf("Seshanba");
+        break;
+     case 3:
+        printf("Chorshanba");
+        break;
+     case 4:
+        printf("Payshanba");
+        break;
+     case 5:
+        printf("Juma");
+        break;
+     case 6:
+        printf("Shanba");
+        break;
+     case 7:
+        printf("Yakshanba");
+        break;
+
+     default:
+        printf("bunday kun haftada yoq");
+        break;
+     }
+}
 
 
 int main(void) {
     mashq1();
     mashq2();
     mashq3();
-
+    mashq4();
     return 0;
 }
