b14292b3a5ffeb9d110c79bfd416d877f737e92b
Abdulboriy <abdulboriy110812@gmail.com>
2026-07-25 19:10:17 +0500

    5 dars

--- main.c
+++ main.c
@@ -1,92 +1,109 @@
 #include <stdio.h>
 
-void mashq1(void) {
+void mashq1(void)
+{
     int age, ball;
-    scanf("%d%d",&age,&ball);
+    scanf("%d%d", &age, &ball);
 
-    if(age >= 18){
+    if (age >= 18)
+    {
 
-      if(ball >= 70){
+        if (ball >= 70)
+        {
 
-        printf("Passed");
-      }else{
-        printf("Failed");
-      }
-      
-    }else{
+            printf("Passed");
+        }
+        else
+        {
+            printf("Failed");
+        }
+    }
+    else
+    {
         printf("Too young");
     }
-
 }
 
+void mashq2(void)
+{
+    int age, ball, atendance;
+    scanf("%d%d%d", &age, &ball, &atendance);
 
-void mashq2(void) {
-    int age, ball,atendance;
-    scanf("%d%d%d",&age,&ball,&atendance);
+    if (age >= 18)
+    {
 
-    if(age >= 18){
-
-      if(ball >= 70){
-        if(atendance >= 90){
-          printf("Congratulations!\nYou passed.\n");         
-        }else{
-            printf("Attendance is too low.\n");
+        if (ball >= 70)
+        {
+            if (atendance >= 90)
+            {
+                printf("Congratulations!\nYou passed.\n");
+            }
+            else
+            {
+                printf("Attendance is too low.\n");
+            }
         }
-      }else{
-        printf("Failed exam\n");
-      }
-      
-    }else{
+        else
+        {
+            printf("Failed exam\n");
+        }
+    }
+    else
+    {
         printf("Too young\n");
     }
 }
 
-void mashq3(void) {
-     int yosh = 18;
-     int xujjat = 1;
+void mashq3(void)
+{
+    int yosh = 18;
+    int xujjat = 1;
 
-     if(yosh >= 18 && xujjat == 1){
+    if (yosh >= 18 && xujjat == 1)
+    {
         printf("Mashina xaydashga ruxsat\n");
-     }else{
+    }
+    else
+    {
         printf("Mashina xaydashga ruxsat emas\n");
-     }
-     
+    }
 }
 
-void mashq4(void){
-     int hafta_kuni = 2;
-     switch (hafta_kuni)
-     {
-     case 1:
+void mashq4(void)
+{
+    int hafta_kuni = 2;
+    switch (hafta_kuni)
+    {
+    case 1:
         printf("Dushanba");
         break;
-     case 2:
+    case 2:
         printf("Seshanba");
         break;
-     case 3:
+    case 3:
         printf("Chorshanba");
         break;
-     case 4:
+    case 4:
         printf("Payshanba");
         break;
-     case 5:
+    case 5:
         printf("Juma");
         break;
-     case 6:
+    case 6:
         printf("Shanba");
         break;
-     case 7:
+    case 7:
         printf("Yakshanba");
         break;
 
-     default:
+    default:
         printf("bunday kun haftada yoq");
         break;
-     }
+    }
 }
 
-
-int main(void) {
+int main(void)
+{
     mashq1();
     mashq2();
     mashq3();
